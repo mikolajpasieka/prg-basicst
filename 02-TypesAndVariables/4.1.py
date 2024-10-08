30f5f200b08ab20c253069ac36d840dfc888fa33
Mikolaj Pasieka <pasiekamikolaj0@gmail.com>
2024-10-08 14:22:56 +0200

    4 skonczona

---
+++ 02-TypesAndVariables/4.1.py
@@ -0,0 +1,7 @@
+###
+# Displaying student's personal data
+#
+name = "Adam"
+age = 19
+height = 180
+print(f"My name is {name}. I am {age} years old, and my height is {height} cm. In 6 years I will be {age + 6} years old.  ")

8fbece2f1485ce2c34aa1b381c5852d72d4819e7
Mikolaj Pasieka <pasiekamikolaj0@gmail.com>
2024-10-08 14:02:08 +0200

    3.2-3.4

---
+++ 02-TypesAndVariables/3.3.py
@@ -0,0 +1,16 @@
+###
+# A program for swapping two varable values
+#
+x = 7
+y = 34
+z = 0 # additional, auxiliary variable
+print("Before swapping:")
+print('x=', x)
+print('y=', y)
+
+# swap the values
+x1 = y
+y1 = x
+print("After swapping:" )
+print("x=", x1)
+print('y=', y1)
---
+++ 02-TypesAndVariables/3.4.py
@@ -0,0 +1,8 @@
+###
+# A program that, for a given speed in km/h,
+# displays the speed in m/s
+#
+speed_kmh = 70
+speed_ms = speed_kmh/3,6
+print("km/h = ", speed_kmh)
+print("m/s=", speed_ms)

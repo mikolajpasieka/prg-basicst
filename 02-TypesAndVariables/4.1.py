f9936f0aaa923efa2dd474d7144db80271040dc7
mikolajpasieka <mikolajpasieka0@gmail.com>
2024-10-13 13:40:54 +0200

    5.4

--- 02-TypesAndVariables/4.1.py
+++ 02-TypesAndVariables/4.1.py
@@ -4,4 +4,4 @@
 name = "Adam"
 age = 19
 height = 180
-print(f"My name is {name}. I am {age} years old, and my height is {height} cm. In 6 years I will be {age + 6} years old.  ")
+print(f"My name is {name}. I am {age} years old, and my height is {height} cm. In 6 years I will be {age + 6} years old.")
--- 02-TypesAndVariables/5.4.py
+++ 02-TypesAndVariables/5.4.py
@@ -1,5 +1,7 @@
-amount = input("type an amount")
-VAT = input("type VAT")
+import math 
+
+amount = int(input("Type an amount:"))
+VAT = int(input("Type VAT:"))
 int(amount)
 int(VAT)
-print(f"The VAT {VAT} % from amount: {amount} is {amount*VAT*0,1*0,1}")
+print(f"The VAT {math.ceil(VAT*100)/100} % from amount: {math.ceil(amount*100)/100} is {math.ceil((amount*VAT*0.01)*100)/100}")

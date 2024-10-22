fc1e0d801bd20cf8312b5bd26d64cd72b5efb7f8
mikolaj pasieka <pasiekamikolaj01@gmail.com>
2024-10-22 14:22:09 +0200

    14:22

--- 04-Functions/5.3.py
+++ 04-Functions/5.3.py
@@ -7,16 +7,18 @@ import keyboard # your own defined module
 
 # Reads employee's data from keyboard
 first_name = input_string('Enter name: ')
-last_name = ...
-age = ...
-salary = ...
+last_name = input_string('Enter lastname: ')
+age = input_real("Enter age:")
+salary = input_real("Enter salary:")
 is_salary_hidden = input_boolean('Hide salary? (y/n)')
 
 # Prints employee's record
 print('DATA RECORD')
 print('===========')
-print('Name:', ...)
-print(...)
-print(...)
-if ...:
-    print('Salary')
+print('Name:', first_name)
+print("Lastname", last_name)
+print("Age:", age)
+if is_salary_hidden == False:
+    print('Salary', salary)
+else:
+    print("Salary hidden")
